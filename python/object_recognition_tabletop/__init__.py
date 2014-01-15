20f3b29b1429efbc9df754fed9df3d44c6cf5eac
Bence Magyar <mw.mzperx@gmail.com>
2014-01-15 22:50:38 +0100

    Added new sink that has a PointCloudMessageAssembler and publishes a RecognizedObjectArray with its results.

--- python/object_recognition_tabletop/__init__.py
+++ python/object_recognition_tabletop/__init__.py
@@ -2,3 +2,4 @@ from object_recognition_tabletop.ecto_cells.tabletop_table import TableDetector
 from .detector import TabletopObjectDetector, TabletopTableDetector
 from .table_publisher import TablePublisher
 from .table_visualizer import TableVisualizer
+from .object_clusters_publisher import ObjectClustersPublisher
